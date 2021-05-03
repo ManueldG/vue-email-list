4daeb022e74ce4e3c7affddfe97759051e38b667
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-04 00:03:08 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -5,45 +5,21 @@ const app = new Vue({
         tmp:[],
     },
     created(){
-        //axios.get('http://casaalmada.hostinggratis.it/API/?nome=gigi&cognome=Rossi') 
-        /*
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-         .then((response) => {
-         // handle success
-            ***************************************
-            /*for ( i in response.data )          *
-             console.log(i+": "+response.data[i]);*/
-             /*************************************/
-            /* console.log(response.data);
-             this.email = response.data.response;
-        })
-        .catch(function (error) {
-        // handle error
-        console.log(error);
-        })
-        .then(function () {
-        // always executed
-
-        });*/
+        
         for (let i=0;i<10;i++){
             this.call();
         };
 
         console.log(this.tmp);
-        this.email = this.tmp;
+        this.email=(this.tmp);
 
     },
     methods:{
         call : function(){
+            // https://flynn.boolean.careers/exercises/api/random/mail
            axios.get('http://casaalmada.hostinggratis.it/API/?nome=gigi&cognome=Rossi')
          .then((response) => {
-         // handle success
-            /*for ( i in response.data )
-             console.log(i+": "+response.data[i]);*/
-            console.log(response.data.response);
-             
-            this.tmp.push(response.data.response);
-            console.log(this.tmp);
+            this.tmp.push(response.data.response);            
         })
         .catch(function (error) {
         // handle error
